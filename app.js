061b7fcb195326679b5b1acc4fb52fe3ad0cd9ee
Mark Somerfield <somerfield.md@gmail.com>
2019-04-15 10:24:28 +0100

    Include client profile cookie

--- app.js
+++ app.js
@@ -92,35 +92,4 @@ app.use(userInViews());
 app.use('/', authRouter);
 app.use('/', indexRouter);
 
-// Catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// Error handlers
-
-// Development error handler
-// Will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// Production error handler
-// No stacktraces leaked to user
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 module.exports = app;
